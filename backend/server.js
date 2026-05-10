b5908b08810477787d535b4264f83f59415014e2
Tesnim hed <tesnissam@gmail.com>
2026-05-10 11:28:11 +0100

    fix bug cors atoken

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization','atoken']
 }))
 
 app.use(express.json({ limit: '10mb' }));
