3ca5991ae7a6d42b40f0e5097f2129848dade94a
Tesnim hed <tesnissam@gmail.com>
2026-05-10 11:32:18 +0100

    ajout des autres tokens

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization','atoken']
+  allowedHeaders: ['Content-Type', 'Authorization','atoken','dtoken','utoken'],
 }))
 
 app.use(express.json({ limit: '10mb' }));
