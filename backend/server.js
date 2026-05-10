88dde260d46849f1d4dc9c683634547e4f0ef069
Tesnim hed <tesnissam@gmail.com>
2026-05-10 19:32:46 +0100

    ajout token

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization','atoken','dtoken','utoken'],
+  allowedHeaders: ['Content-Type', 'Authorization','atoken','dtoken','utoken','token'],
 }))
 
 app.use(express.json({ limit: '10mb' }));
